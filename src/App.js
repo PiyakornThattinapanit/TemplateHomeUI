7c4d4edf9a55abf661ed9371f368c0bf03266777
Piyakorn Thattinapanich <big_v7@hotmail.com.com>
2023-12-06 14:01:54 +0700

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,136 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import "./App.css";
+import {Menu} from "antd";
+import {Routes,Route,useNavigate} from "react-router-dom";
+import {HomeOutlined,
+  DashboardOutlined,
+  UnorderedListOutlined,
+  UserOutlined,
+  PoweroffOutlined
+} from "@ant-design/icons"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const navigate = useNavigate()
+  
+  function SideMenu() {
+    return (
+      <Menu
+      onClick={({key}) => {
+        if (key === "signout"){
+          console.log("signout Popup");
+        } else {
+          navigate(key);
+        }
+      }}
+        defaultSelectKeys={[window.location.pathname]}
+        items={[
+          {
+            label: "Home",
+            key:"/",
+            icon: <HomeOutlined/>,
+          },
+          {
+            label: "Dashboard",
+            key:"/dashboard",
+            icon: <DashboardOutlined/>
+          },
+          { label: "Users List",
+            key:"/userslist",
+            icon: <UnorderedListOutlined/>,
+            children: [
+              {
+                label: "Active USers",
+                key: "/activeUsers"
+              },
+              {
+                label: "Disabled users",
+                key: "/disableUsers"
+              }
+            ]
+          },  
+          { label: "Profile",
+            key:"/profile",
+            icon: <UserOutlined/>
+          },
+          {
+            label: "Signout",
+            key:"signout",
+            icon: <PoweroffOutlined/>,
+            danger:true
+          },
+        ]}
+      ></Menu>
+    )
+  }
+  
+  function Content() {
+    return <div>
+      <Routes>
+        <Route path='/' element={<div>Home</div>}></Route>
+        <Route path='/dashboard' element={<div>Dashboard</div>}></Route>
+        <Route path='/activeUsers' element={<div>Active Users list</div>}></Route>
+        <Route path='/disableUsers' element={<div>Disabled Users list</div>}></Route>
+        <Route path='/profile' element={<div>Profile</div>}></Route>
+      </Routes>
     </div>
-  );
+  }
+  function Header() {
+    return (
+    <div 
+      style={{
+        height:80,
+        backgroundColor: '#8ADAB2',
+        color:'brown',
+        display:'flex',
+        justifyContent:'center',
+        alignItems:"center",
+        fontWeight: "bold"
+      }}
+    >
+      HOME
+    </div>
+    );
+  }
+  function Footer() {
+    return (
+    <div
+      style={{
+        height:60,
+        backgroundColor: "#D0F288",
+        color: "brown",
+        display: 'flex',
+        justifyContent: "center",
+        justifyItems: "center",
+        fontWeight: "bold"
+      }}
+    >
+      Footer
+    </div>);
+  }
+
+  return (
+    <div 
+      style={{
+        display:"flex",
+        flexDirection: "column",
+        flex: 1,
+        height: '100vh'
+      }}
+    >
+      <Header/>
+      <div 
+        style={{
+          display:"flex", 
+          flexDirection: "row",
+          flex: 1
+        }}
+      >
+        <SideMenu/>
+        <Content/>
+      </div>
+      <Footer/>
+    </div>
+  )
 }
 
-export default App;
+export default App
